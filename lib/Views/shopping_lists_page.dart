d70d9cde2c8f93b51143ce8ef8e285d533b2ae1f
klikman1 <mmihigojonathan@gmail.com>
2025-05-06 22:15:40 +0200

    Created a shopping list card, page dedicated to shopping lists and a list details page

--- lib/Views/shopping_lists_page.dart
+++ lib/Views/shopping_lists_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:techno_mobile/Widgets/new_list_popup.dart';
+import 'package:techno_mobile/Widgets/shopping_list_card.dart';
 
 class ShoppingListsPage extends StatefulWidget {
   const ShoppingListsPage({super.key});
@@ -11,68 +12,135 @@ class ShoppingListsPage extends StatefulWidget {
 }
 
 class ShoppingListsPageState extends State<ShoppingListsPage> {
-  List<String> shoopingLists = [];
+  List<String> shoppingLists = [
+    "Weekly Groceries",
+    "Party Prep",
+    "Office Supplies",
+    "Weekly Groceries",
+    "Party Prep",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    if (shoopingLists.isEmpty) {
+    // ---------------There is no groceries list created yet --------
+    if (shoppingLists.isEmpty) {
       return Scaffold(
-          body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          //----------------The title of this page------------------------
-          Text(
-            "Your shopping Lists",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
+        appBar: AppBar(),
+        backgroundColor: Colors.grey.shade100,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            //----------------The title of this page------------------------
+            Text(
+              "Your shopping Lists",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
 
-          //-------------------The body-----------------------------------
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/cart.png',
-                  height: 150, // Adjust size as needed
+            //-------------------The body-----------------------------------
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/cart.png',
+                    height: 150,
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    "You have not added any shopping lists, Tap the button below to create one now",
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18.0),
+                  )
+                ],
+              ),
+            ),
+
+            //-------------------The create button---------------------------
+            TextButton.icon(
+              onPressed: () =>
+                  showDialog(
+                    context: context,
+                    builder: (context) => const NewListPopup(),
+                  ),
+              label: Text(
+                "Create",
+                style: TextStyle(color: Colors.white, fontSize: 18.0),
+              ),
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.lightGreen.shade600,
+                  padding: EdgeInsets.symmetric(horizontal: 25.0)),
+            )
+          ],
+        ),
+      );
+    }
+
+    //------------------The groceries list isn't empty---------------------
+    return Scaffold(
+
+      //-----------------------Title--------------------------
+      appBar: AppBar(
+        title: const Text(
+          "Your Shopping Lists",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.grey.shade100,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              //--------------------Groceries list --------------------
+
+              Flexible(
+                child: ListView.builder(
+                  itemCount: shoppingLists.length,
+                  itemBuilder: (context, index) {
+                    return ShoppingListCard(listName: shoppingLists[index]);
+                  },
                 ),
-                SizedBox(height: 12.0),
-                Text(
-                  "You have not added any shopping lists, Tap the button below to create one now",
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                  style: TextStyle(fontSize: 18.0),
-                )
-              ],
-            ),
+              ),
+            ],
           ),
-
-          //-------------------The create button---------------------------
-          TextButton.icon(
-            onPressed: () => showDialog(
+        ),
+      ),
+      floatingActionButton: //--------------------Create button ----------------------
+      TextButton.icon(
+        onPressed: () =>
+            showDialog(
               context: context,
               builder: (context) => const NewListPopup(),
             ),
-            label: Text(
-              "Create",
-              style: TextStyle(color: Colors.white, fontSize: 18.0),
-            ),
-            icon: Icon(
-              Icons.add,
-              color: Colors.white,
-            ),
-            style: TextButton.styleFrom(
-                backgroundColor: Colors.lightGreen.shade600,
-                padding: EdgeInsets.symmetric(horizontal: 25.0)),
-          )
-        ],
-      ));
-    }
-
-    return Container();
+        label: Text(
+          "Create",
+          style: TextStyle(color: Colors.white, fontSize: 18.0),
+        ),
+        icon: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.lightGreen.shade600,
+            padding: EdgeInsets.symmetric(horizontal: 25.0)),
+      ),
+    );
   }
 }
---
+++ lib/Widgets/shopping_list_card.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:techno_mobile/Views/list_details_page.dart';
+
+class ShoppingListCard extends StatefulWidget {
+  final String listName;
+
+  const ShoppingListCard({super.key, required this.listName});
+
+  @override
+  State<StatefulWidget> createState() {
+    return ShoppingListCardState();
+  }
+}
+
+class ShoppingListCardState extends State<ShoppingListCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6.0,
+      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: ListTile(
+        contentPadding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 10.0),
+        //------------- List title and its products-----------------------
+        leading: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.listName,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 4.0),
+            Text(
+              "rice, bread, plantain + 13 more",
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.black54,
+              ),
+            )
+          ],
+        ),
+
+        //------------------------Semi-round count box ------------------
+        trailing: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12.0,
+            vertical: 8.0,
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.green, width: 2.0),
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: const Text(
+            "4/4",
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+
+        onTap: (){
+          // Navigates to the list details page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ListDetailsPage(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
